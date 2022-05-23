a081ff6a2f199dc45383358b921c0ed2692d582b
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-23 17:55:06 +0600

    mongodb connect success

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wvwif.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db("CarManufacturers").collection("parts");
+
+    app.get("/parts", async (req, res) => {
+      const query = {};
+      const carsor = partsCollection.find(query);
+      const result = await carsor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("our server is running successfully ");
