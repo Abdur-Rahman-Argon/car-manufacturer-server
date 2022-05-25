72f598900565868dceae5b661a575999a11e67e8
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-25 20:07:58 +0600

    review get  api added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -19,7 +20,7 @@ function verifyJWT(req, res, next) {
     if (err) {
       return res.status(403).send({ message: "Forbidden Access" });
     }
-    console.log(token, decoded, err);
+    // console.log(token, decoded, err);
     req.decoded = decoded;
     next();
   });
@@ -82,6 +83,16 @@ async function run() {
       }
     });
 
+    app.get("/parches/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const orders = await parchesCollection.findOne(query);
+      // const orders = await cursor.toArray();
+      // const part = orders.find((itm) => itm._id == id);
+      // console.log(id, orders);
+      res.send(orders);
+    });
+
     app.post("/parches", verifyJWT, async (req, res) => {
       const parches = req.body;
       const result = await parchesCollection.insertOne(parches);
@@ -92,6 +103,13 @@ async function run() {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send({ success: true, result });
+    });
+
+    app.get("/review", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
     app.put("/user/:email", async (req, res) => {
