a584aca2a7a0cbc7333df49822ea456cdbe2255b
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-26 06:48:00 +0600

    delete order api create

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -83,7 +83,7 @@ async function run() {
       }
     });
 
-    app.get("/parches/:id", verifyJWT, async (req, res) => {
+    app.get("/parches/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const orders = await parchesCollection.findOne(query);
@@ -93,10 +93,29 @@ async function run() {
       res.send(orders);
     });
 
+    app.delete("/parches/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await parchesCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/parches", verifyJWT, async (req, res) => {
       const parches = req.body;
       const result = await parchesCollection.insertOne(parches);
       res.send({ success: true, result });
+    });
+
+    app.post("/create-payment", async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
     });
 
     app.post("/review", async (req, res) => {
